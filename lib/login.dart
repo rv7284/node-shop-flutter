4599768c7bfc9e1cfa528229d8d1308946e11072
Ravi Goswami <ravi.devstree@gmail.com>
2019-05-13 18:59:08 +0530

    login button animation

--- lib/login.dart
+++ lib/login.dart
@@ -38,25 +38,35 @@ class _LoginState extends State<Login> {
                   hasFloatingPlaceholder: true, hintText: 'Password'),
             ),
             SizedBox(height: 100.0),
-            RaisedButton(
-              color: Theme.of(context).primaryColor,
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 40.0, vertical: 12.0),
-              child: Text(
-                'Login',
-                style: TextStyle(color: Colors.white, fontSize: 20.0),
-              ),
-              onPressed: () {
-                String email = _emailController.text;
-                String password = _passwordController.text;
+            StreamBuilder<bool>(
+              initialData: false,
+              stream: widget.userBloc.isLoading,
+              builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+                if (snapshot.data) {
+                  return CircularProgressIndicator();
+                } else {
+                  return RaisedButton(
+                    color: Theme.of(context).primaryColor,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40.0, vertical: 12.0),
+                    child: Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                    onPressed: () {
+                      String email = _emailController.text;
+                      String password = _passwordController.text;
 
-                if (email.isNotEmpty && password.isNotEmpty) {
-                  UserCredentials credentials =
-                      UserCredentials(email, password);
-                  widget.userBloc.userAuth.add(credentials);
+                      if (email.isNotEmpty && password.isNotEmpty) {
+                        UserCredentials credentials =
+                            UserCredentials(email, password);
+                        widget.userBloc.userAuth.add(credentials);
+                      }
+                    },
+                  );
                 }
               },
-            )
+            ),
           ],
         ),
       ),
--- lib/user_bloc.dart
+++ lib/user_bloc.dart
@@ -22,11 +22,16 @@ class UserBloc {
   Stream<String> get userToken => _loginStatusSubject.stream;
   final _loginStatusSubject = BehaviorSubject<String>();
 
+  Stream<bool> get isLoading => _isLoading.stream;
+  final _isLoading = BehaviorSubject<bool>();
+
   Sink<UserCredentials> get userAuth => _userController.sink;
   final _userController = StreamController<UserCredentials>();
 
   void close() {
     _userController.close();
+    _loginStatusSubject.close();
+    _isLoading.close();
   }
 
   static const _baseUrl = 'https://rv7284-node-shop-api.herokuapp.com';
@@ -42,12 +47,13 @@ class UserBloc {
         _loginStatusSubject.sink.add(token);
       }
     });
-
     _userController.stream.listen((userCredentials) async {
+      _isLoading.sink.add(true);
       var response = await http.post(_baseUrl + '/user/login',
           body: userCredentials.json());
       var jsonResponse = convert.jsonDecode(response.body);
       String token = jsonResponse['token'];
+      _isLoading.sink.add(false);
       _loginStatusSubject.sink.add(token);
     });
   }
